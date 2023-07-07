6fdf5d2f154023c56235c454df7978dc8b25a3c5
Crvstal8100 <99895952+Crvstal8100@users.noreply.github.com>
2023-07-07 19:46:37 +0200

    Create Documentation.lua

---
+++ Documentation.lua
@@ -0,0 +1,20 @@
+-- Load UI Lib
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Crvstal8100/UiLib/main/Lib.lua"))()
+
+-- Create UI
+local UI = Library:Create(name: string)
+
+-- Create Tab
+local Tab = UI:Create(name: string)
+
+-- Create Button
+local Button = Tab:CreateButton(name: string, callback: function)
+
+-- Create Toggle
+local Toggle = Tab:CreateToggle(name: string, state: bool, callback: function)
+
+-- Create TextBox
+local TextBox = Tab:CreateTextBox(placeholder: string, text: string, callback: function)
+
+-- Create Slider
+local Slider = Tab:CreateSlider(name: string, minvalue: number, maxvalue: number, callback:function)
