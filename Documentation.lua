441169308f9833c8eea5528f34d55dc4ba0d1e26
Crvstal8100 <99895952+Crvstal8100@users.noreply.github.com>
2023-07-07 20:04:33 +0200

    Update Documentation.lua

--- Documentation.lua
+++ Documentation.lua
@@ -18,3 +18,36 @@ local TextBox = Tab:CreateTextBox(placeholder: string, callback: function)
 
 -- Create Slider
 local Slider = Tab:CreateSlider(name: string, minvalue: number, maxvalue: number, callback:function)
+
+
+-- Examples:
+
+-- Load UI Lib
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Crvstal8100/UiLib/main/Lib.lua"))()
+
+-- Create UI
+local UI = Library:Create("Balls UI")
+
+-- Create Tab
+local Tab = UI:Create("Target")
+
+-- Create Button
+local Button = Tab:CreateButton("Sex?", function()
+    print("No.")
+end)
+
+-- Create Toggle
+local Toggle = Tab:CreateToggle("AFK", false, function(state)
+    print(state)
+end)
+
+-- Create TextBox
+local TextBox = Tab:CreateTextBox("Username", function(text)
+    print(text)
+end)
+
+-- Create Slider
+local Slider = Tab:CreateSlider("Zestiness", 0, 100, function(value)
+    print(value)
+end)
+
