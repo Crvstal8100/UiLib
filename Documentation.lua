052836eb9b167195bd89b4fa789b55298b10022a
Crvstal8100 <99895952+Crvstal8100@users.noreply.github.com>
2023-07-08 13:39:12 +0200

    Update Documentation.lua

--- Documentation.lua
+++ Documentation.lua
@@ -14,11 +14,23 @@ local Button = Tab:CreateButton(name: string, callback: function)
 local Toggle = Tab:CreateToggle(name: string, state: bool, callback: function)
 
 -- Create TextBox
-local TextBox = Tab:CreateTextBox(placeholder: string, callback: function)
+local TextBox = Tab:CreateTextBox(name: string, placeholder: string, callback: function)
 
 -- Create Slider
 local Slider = Tab:CreateSlider(name: string, minvalue: number, maxvalue: number, callback:function)
 
+-- Other
+
+-- Toggle
+local State = Toggle:GetState() -- returns State
+Toggle:SetState(state: bool) -- sets State to bool
+
+-- TextBox
+local Text = TextBox:GetText() -- returns Text
+Text:SetText(text: string)
+
+-- Slider
+local Value = Slider:GetValue() -- returns Value
 
 -- Examples:
 
@@ -32,22 +44,31 @@ local UI = Library:Create("Balls UI")
 local Tab = UI:Create("Target")
 
 -- Create Button
-local Button = Tab:CreateButton("Sex?", function()
-    print("No.")
+local Button = Tab:CreateButton("Button1", function()
+    print("Hello World.")
 end)
 
 -- Create Toggle
-local Toggle = Tab:CreateToggle("AFK", false, function(state)
+local Toggle = Tab:CreateToggle("Toggle1", false, function(state)
     print(state)
 end)
 
 -- Create TextBox
-local TextBox = Tab:CreateTextBox("Username", function(text)
+local TextBox = Tab:CreateTextBox("TextBox1", "...", function(text)
     print(text)
 end)
 
 -- Create Slider
-local Slider = Tab:CreateSlider("Zestiness", 0, 100, function(value)
+local Slider = Tab:CreateSlider("Slider1", 0, 100, function(value)
     print(value)
+end)
+
+-- Other
+local Button2 = Tab:CreateButton("Button2", function()
+    print(Toggle:GetState())
+end)
+
+local Button3 = Tab:CreateButton("Button3", function()
+    print(Toggle:SetState(true))
 end)
 
